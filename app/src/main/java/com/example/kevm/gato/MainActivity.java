0c4aebf3f9131067c1b965184eb6392ade4855c4
TheRaven0 <33192268+TheRaven0@users.noreply.github.com>
2018-05-11 00:18:16 -0500

    Vinculadas referencias con vista. Aplicado el método on click a los botones, aunque todavía no se define.

--- app/src/main/java/com/example/kevm/gato/MainActivity.java
+++ app/src/main/java/com/example/kevm/gato/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.kevm.gato;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public Button btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_reset;
+    public int jugador, turno;
+    public EditText resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultado = (EditText)(findViewById(R.id.resultado));
+
+        btn_1 = (Button)(findViewById(R.id.btn1));
+        btn_2 = (Button)(findViewById(R.id.btn2));
+        btn_3 = (Button)(findViewById(R.id.btn3));
+        btn_4 = (Button)(findViewById(R.id.btn4));
+        btn_5 = (Button)(findViewById(R.id.btn5));
+        btn_6 = (Button)(findViewById(R.id.btn6));
+        btn_7 = (Button)(findViewById(R.id.btn7));
+        btn_8 = (Button)(findViewById(R.id.btn8));
+        btn_9 = (Button)(findViewById(R.id.btn9));
+        btn_reset = (Button)(findViewById(R.id.reset));
+
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_reset.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
